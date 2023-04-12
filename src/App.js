3cbdf3b3ff5aa39dd32c9c42ba4a22c91934c56a
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-04-12 17:23:13 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Navbar from './Navbar';
 function App() {
   return (
     <div>
-      Initial Commit
+      <Navbar/>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react';
+import './Navbar.css';
+
+function Navbar() 
+{
+    const [navbar, setNavbar]=useState(false);
+    const showShadow =() => 
+    {
+      if (window.scrollY>5)
+      {
+        setNavbar(true);
+      }
+      else
+      {
+        setNavbar(false);
+      }
+    }
+    window.addEventListener('scroll',showShadow);
+
+  return (
+    <div>
+      <nav className={navbar? 'navbar-container navbar-shadow':'navbar-container'}>
+        <a href='http://localhost:3000/' className='logo-container'>
+          <div></div>
+        </a>
+        <div className='navigation-links'>
+            <ol className='ordered-list'>
+                <li><a href=''>About</a></li>
+                <li><a href=''>Experience</a></li>
+                <li><a href=''>Work</a></li>
+                <li><a href=''>Contact</a></li>
+            </ol>
+
+            <a href='' className='resume-button'>Resume</a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
+

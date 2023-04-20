1ea6a9ef48a7366b8460fbba942ac92d0fc99517
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-04-20 15:24:09 +0530

    contact animation on scroll added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,38 @@ import '../styles/Contact.css';
 function Contact() 
 {
 
+    const contactRef = useRef(null);
+    useEffect(() => 
+    {
+        const options = 
+        {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.7
+        };
+
+        const observer = new IntersectionObserver((entries, observer) => 
+        {
+            entries.forEach(entry => 
+                {
+                    if (entry.isIntersecting) 
+                    {
+                        entry.target.classList.add('show');
+                    }
+                });
+        }, options);
+
+        if (contactRef.current) observer.observe(contactRef.current);
+
+        return () => 
+        {
+            if (contactRef.current) observer.unobserve(contactRef.current);
+        };
+    }, []);
+
     return (
         <div>
-            <section className='Contact_Container'>
+            <section className='Contact_Container' ref={contactRef}>
 
                 <h1>Get In Touch</h1>
                 <p>Although I'm not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!</p>

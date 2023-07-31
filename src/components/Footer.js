389af5f43ebe7df217f0b4a904c33ca73f5a96e0
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-08-01 05:00:19 +0530

    social icons added in footer and some other ui tweaks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,25 @@
 import React, { useRef, useEffect } from 'react';
 import '../index.css';
 import '../styles/Footer.css';
-
+import { ReactComponent as Github } from '../icons/github.svg';
+import { ReactComponent as Linkedin } from '../icons/linkedin.svg';
+import { ReactComponent as Instagram } from '../icons/instagram.svg';
+import { ReactComponent as Twitter } from '../icons/twitter.svg';
+import { ReactComponent as Codepen } from '../icons/codepen.svg';
+import { ReactComponent as Leetcode } from '../icons/leetcode.svg';
 
 function Footer() 
 {
 
     return (
         <section className='footer_container'>
+            <ul>
+                <li><Github/></li>
+                <li><Codepen/></li>
+                <li><Linkedin/></li>
+                <li><Instagram/></li>
+                <li><Twitter/></li>
+            </ul>
             <a href='https://github.com/ani1609/resume' target='_blank' >Designed & Built by Ankit Kr. Chowdhury</a>
         </section>
     );
